24e0127ddca9bfea893ccc61ef3e7ec9a237741d
Harry2k21 <parker2002hp@gmail.com>
2023-05-31 20:41:15 +0100

    update structure

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import '../components/App.css';
+// import express from express;
+// const app = express();
+
+function App() {
+  return (
+    <div>
+     <h1>Vision Tool AI</h1>
+    </div>
+  );
+}
+
+export default App;
