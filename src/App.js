d573df16f2e45e960eb08f7dea61c7bc1013ee67
Lee Webb <leewerbb55@hotmail.com>
2023-06-01 03:15:42 +0800

    Very early proof that Google Lens works - app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [imageUrl, setImageUrl] = useState('');
+  const [imageInfo, setImageInfo] = useState('');
+
+  const handleImageUpload = async (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onloadend = async () => {
+      const base64data = reader.result;
+      setImageUrl(base64data);
+
+
+      // ENTER YOUR API KEY WHERE IT SAYS YOUR_API_KEY 
+      try {
+        const response = await fetch('https://vision.googleapis.com/v1/images:annotate?key=YOUR_API_KEY', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            requests: [
+              {
+                image: {
+                  content: base64data.split(',')[1],
+                },
+                features: [
+                  {
+                    type: 'WEB_DETECTION',
+                    maxResults: 5,
+                  },
+                  {
+                    type: 'LABEL_DETECTION',
+                    maxResults: 5,
+                  },
+                ],
+              },
+            ],
+          }),
+        });
+
+        const data = await response.json();
+        setImageInfo(JSON.stringify(data, null, 2));
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    };
+
+    reader.readAsDataURL(file);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Google Lens Image Analysis</h1>
+      <input type="file" accept="image/*" onChange={handleImageUpload} />
+      {imageUrl && <img src={imageUrl} alt="Uploaded" width="300" />}
+      {imageInfo && (
+        <pre>
+          <code>{imageInfo}</code>
+        </pre>
+      )}
     </div>
   );
-}
+};
 
 export default App;
