2467a677c66c37721dd9bb09ccf9f7975995f029
Harry2k21 <parker2002hp@gmail.com>
2023-06-02 19:39:12 +0100

    no changes

--- src/components/App.js
+++ src/components/App.js
@@ -107,7 +107,7 @@ const App = () => {
     <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
       <div className="relative py-3 sm:max-w-xl sm:mx-auto">
         <div className="absolute inset-0 bg-gradient-to-r from-cyan-400 to-light-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
-        <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+        <div className=" w-200 md:w-200 lg:w-200 relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
           <h1 className="text-2xl font-bold mb-4">Vision AI - Image-to-Recipe Analysis</h1>
           <p className="mb-4">Upload an image to get a recipe and nutritional information based on the image's content.</p>
           <input type="file" accept="image/*" onChange={handleImageUpload} className="mb-4" />
