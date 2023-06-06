f44f866fd860e9be5f449f840e4a4d6f41097dd8
Harry2k21 <parker2002hp@gmail.com>
2023-06-06 20:10:09 +0100

    spin cube added

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import '../index.css';
+import '../index.css'
+import './App.scss'
 
 // Load environment variables
 const googleLensApiKey = process.env.REACT_APP_GOOGLE_LENS_API_KEY;
@@ -103,10 +104,25 @@ const App = () => {
     }
   };
 
+
   return (
-    <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
+    
+    <div className="min-h-screen bg-blue-100 py-6 flex flex-col justify-center sm:py-12">
+      <div class="container" id="load">
+  <div class="cube">
+    <div class="sides">
+      <div class="top"></div>
+      <div class="right"></div>
+      <div class="bottom"></div>
+      <div class="left"></div>
+      <div class="front"></div>
+      <div class="back"></div>
+    </div>
+  </div>
+  <div class="text">BUNDLING DEPENDENCIES</div>
+</div>
       <div className="relative py-3 sm:max-w-xl sm:mx-auto">
-        <div className="absolute inset-0 bg-gradient-to-r from-cyan-400 to-light-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
+        <div className="absolute inset-0 bg-gradient-to-r from-purple-400 to-light-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
         <div className=" w-200 md:w-200 lg:w-200 relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
           <h1 className="text-2xl font-bold mb-4">Vision AI - Image-to-Recipe Analysis</h1>
           <p className="mb-4">Upload an image to get a recipe and nutritional information based on the image's content.</p>
@@ -116,6 +132,7 @@ const App = () => {
             <div>
               <h2 className="text-xl font-bold mb-2">Recipe and Nutritional Information:</h2>
               <div className="whitespace-pre-wrap break-words">{recipe}</div>
+              <div></div>
             </div>
           )}
         </div>
