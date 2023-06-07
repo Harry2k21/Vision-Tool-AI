3a99c0c92aa3542b7f910a78fc91d34d7d22f208
Lee Webb <leewerbb55@hotmail.com>
2023-06-08 02:49:15 +0800

    Added Image Generation Tab

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -52,55 +52,85 @@ function HomePage({ darkMode, handleImageUpload, imageUrl, recipe, isLoading }) 
           <div className="text">Loading</div>
         </div>
       )}
-        <div className="relative py-3 sm:max-w-xl sm:mx-auto">
-          <div
-            className={`absolute inset-0 ${
-              darkMode ? "bg-gradient-to-r from-gray-800" : "bg-gradient-to-r from-cyan-400"
-            } to-light-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl`}
-          ></div>
-          <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
-            <h1 className="text-2xl font-bold mb-4">
-              Vision AI - Image-to-Recipe Analysis
-            </h1>
-            <p className="mb-4">
-              Upload an image to get a recipe and nutritional information based
-              on the image's content.
-            </p>
-            <input
-              type="file"
-              accept="image/*"
-              onChange={handleImageUpload}
-              className="mb-4"
-            />
-            {imageUrl && (
-              <img
-                src={imageUrl}
-                alt="Uploaded"
-                width="300"
+      <div className="relative py-3 sm:max-w-xl sm:mx-auto">
+        <div
+          className={`absolute inset-0 ${
+            darkMode ? "bg-gradient-to-r from-gray-800" : "bg-gradient-to-r from-cyan-400"
+          } to-light-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl`}
+        ></div>
+        <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+          <h1 className="text-2xl font-bold mb-4">
+            Vision AI - Image-to-Recipe Analysis
+          </h1>
+          {/* Add tabs to switch between upload image and generate image */}
+          <div className="flex mb-4">
+            <button
+              onClick={() => setActiveTab('upload')}
+              className={`font-bold py-2 px-4 rounded ${
+                activeTab === 'upload'
+                  ? 'bg-blue-500 text-white'
+                  : 'text-blue-500'
+              }`}
+            >
+              Upload Image
+            </button>
+            <button
+              onClick={() => setActiveTab('generate')}
+              className={`font-bold py-2 px-4 rounded ml-4 ${
+                activeTab === 'generate'
+                  ? 'bg-blue-500 text-white'
+                  : 'text-blue-500'
+              }`}
+            >
+              Generate Image
+            </button>
+          </div>
+          {activeTab === 'upload' ? (
+            <div>
+              <p className="mb-4">
+                Upload an image to get a recipe and nutritional information based
+                on the image's content.
+              </p>
+              <input
+                type="file"
+                accept="image/*"
+                onChange={handleImageUpload}
                 className="mb-4"
               />
-            )}
-            {recipe && (
-              <div>
-                <h2 className="text-xl font-bold mb-2">
-                  Recipe and Nutritional Information:
-                </h2>
-                <div className="whitespace-pre-wrap break-words">{recipe}</div>
-              </div>
-            )}
-            <button
-                onClick={() => handleSaveRecipe(recipe)}
-                className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-              >
-                Save Recipe
-              </button>
-              <button
-              onClick={() => handleShareRecipe(recipe)}
-              className="mt-4 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
-              >
-              Share Recipe
-              </button>
-            <div className="scroll-to-top-button-container">
+              {imageUrl && (
+                <img
+                  src={imageUrl}
+                  alt="Uploaded"
+                  width="300"
+                  className="mb-4"
+                />
+              )}
+              {recipe && (
+                <div>
+                  <h2 className="text-xl font-bold mb-2">
+                    Recipe and Nutritional Information:
+                  </h2>
+                  <div className="whitespace-pre-wrap break-words">{recipe}</div>
+                  {/* Save and share recipe buttons */}
+                  <button
+                    onClick={() => handleSaveRecipe(recipe)}
+                    className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2"
+                  >
+                    Save Recipe
+                  </button>
+                  <button
+                    onClick={() => handleShareRecipe(recipe)}
+                    className="mt-4 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+                  >
+                    Share Recipe
+                  </button>
+                </div>
+              )}
+            </div>
+          ) : (
+            <ImageGenerationTab darkMode={darkMode} apiKey={process.env.REACT_APP_OPENAI_API_KEY} />
+          )}
+          <div className="scroll-to-top-button-container">
             <ScrollToTopButton />
           </div>
         </div>
