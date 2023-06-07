072f33be9c183f89480af688728a34f0e6c15965
Harry2k21 <parker2002hp@gmail.com>
2023-06-07 19:24:54 +0100

    Changed Background

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -21,7 +21,7 @@ function HomePage({ darkMode, handleImageUpload, imageUrl, recipe }) {
         <div className="relative py-3 sm:max-w-xl sm:mx-auto">
           <div
             className={`absolute inset-0 ${
-              darkMode ? "bg-gradient-to-r from-gray-800" : "bg-gradient-to-r from-cyan-400"
+              darkMode ? "bg-gradient-to-r from-gray-800" : "bg-gradient-to-r from-purple-400"
             } to-light-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl`}
           ></div>
           <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
